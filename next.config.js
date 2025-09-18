5d4e79e169ed1df25e4bfc215c6182177824d3a1
bssigno1815 <briyantsigno@gmail.com>
2025-09-18 17:27:08 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // keep it minimal — no output, no distDir, no experimental flags
+  eslint: { ignoreDuringBuilds: true }
 };
-
 module.exports = nextConfig;
