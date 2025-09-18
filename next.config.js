8a3e85aaa80d406e88b4660d383f1a75ab720a16
bssigno1815 <briyantsigno@gmail.com>
2025-09-18 11:47:28 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // add domains if you’ll load images from outside your repo
-  },
-  env: {
-    BRAND_NAME: "Briyant Solèy Signo 1815",
-    BRAND_COLORS: "Apricot Orange (#FFA500) & Black (#000000)",
-    BRAND_SLOGAN: "Yon motè nèf, yon sèl kout klé san bri"
-  }
-}
-
-module.exports = nextConfig
+  reactStrictMode: true
+};
+module.exports = nextConfig;
