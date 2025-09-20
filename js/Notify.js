ad264f01d7c5f47ff502948272c114b1e9747072
bssigno1815 <briyantsigno@gmail.com>
2025-09-20 19:58:16 -0400

    Update Notify.js

--- js/Notify.js
+++ js/Notify.js
@@ -1,4 +1,14 @@
-window.bssNotify = {
+fetch('/api/notify-email', {
+  method:'POST',
+  headers:{'Content-Type':'application/json'},
+  body: JSON.stringify({ subject:'Test', text:'Email OK?' })
+}).then(r=>r.json()).then(console.log);
+
+fetch('/api/notify-sms', {
+  method:'POST',
+  headers:{'Content-Type':'application/json'},
+  body: JSON.stringify({ text:'SMS OK?' })
+}).then(r=>r.json()).then(console.log);window.bssNotify = {
   async send({subject, text, html, toEmail, toPhone}) {
     // Email
     await fetch('/api/notify-email', {
