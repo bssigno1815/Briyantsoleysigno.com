cc0bc0930afb289f96f660ee8c8fc40a9a9307ff
bssigno1815 <briyantsigno@gmail.com>
2025-09-22 13:25:40 -0400

    Create page/istwa.tsx

---
+++ page/istwa.tsx
@@ -0,0 +1,15 @@
+import IstwaSection from "@/components/IstwaSection";
+
+export default function Page() {
+  return (
+    <main>
+      {/* Intro bann lan */}
+      <IntroSection />
+
+      {/* Istwa Bann lan */}
+      <IstwaSection />
+
+      {/* Dirijan, Mizisyen, Fanatik, elatriye */}
+    </main>
+  );
+}
