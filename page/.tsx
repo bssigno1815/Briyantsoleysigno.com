c3ae054e79f55c4239abbf7db5e2a7b2a77535a6
bssigno1815 <briyantsigno@gmail.com>
2025-09-22 14:56:42 -0400

    Update .tsx

--- page/.tsx
+++ page/.tsx
@@ -1,4 +1,23 @@
-import IstwaSection from "@/components/IstwaSection";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = { title: "BSS 1815", description: "Briyant Solèy Signo 1815" };
+
+export default function RootLayout({ children, params:{locale} }:{
+  children: React.ReactNode; params:{locale:string}
+}) {
+  return (
+    <html lang={locale}>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}import IstwaSection from "@/components/IstwaSection";
 
 export default function Page() {
   return (
