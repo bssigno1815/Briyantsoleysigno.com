bb2b2ddfea309b7e4e1b65f55ed494c326758487
bssigno1815 <briyantsigno@gmail.com>
2025-09-21 22:58:56 -0400

    Create dirijan.tsx

---
+++ dirijan.tsx
@@ -0,0 +1,10 @@
+import CommitteeSection from "@/components/CommitteeSection";
+
+export default function DirijanPage() {
+  return (
+    <>
+      {/* lòt kontni ou yo… */}
+      <CommitteeSection />
+    </>
+  );
+}
