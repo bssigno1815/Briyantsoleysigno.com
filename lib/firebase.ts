d8b2ffd581af3a93fbc6eee76bd8766a1ad1093f
bssigno1815 <briyantsigno@gmail.com>
2025-09-23 15:05:01 -0400

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,13 @@
-// lib/firebase.ts
+import { initializeApp, getApps } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);// lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
