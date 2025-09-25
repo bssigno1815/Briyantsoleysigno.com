31fdd6999228b2ea8a93c0f59ee3a453896a8e31
bssigno1815 <briyantsigno@gmail.com>
2025-09-25 13:14:52 -0400

    Delete page/text directory

--- page/text/orange-black.tsx
+++
@@ -1,4 +0,0 @@
-<div className="rounded-2xl border border-orange p-4 bg-gradient-to-b from-[#0a0a0a] to-[#000]">
-  <div className="text-orange font-semibold">...</div>
-  <div className="text-orange/90 text-sm">...</div>
-</div>
