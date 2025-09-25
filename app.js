d4737e7d972ed39a8a90678fb7be7665a8e1e6ac
bssigno1815 <briyantsigno@gmail.com>
2025-09-25 04:16:11 -0400

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// Sign-out handler (if present)
-document.getElementById('signOutLink')?.addEventListener('click', async (e)=>{
-  e.preventDefault();
-  try{ await window.bss.signOut(window.bss.auth); }catch{}
-  location.href='/index.html';
-});
